e7be3eb0784c76d516fb64b3affeb726028b220a
Andres <andres-eloy.rubio-carvajal@ensta-paris.fr>
2021-02-28 08:46:15 +0100

    Exercice 1

--- bataille-navale/src/main/java/ensta/Board.java
+++ bataille-navale/src/main/java/ensta/Board.java
@@ -19,14 +19,18 @@ public class Board
         this(name,10);
     }
 
-    public void print(){
-
-
-
-
+    private void printBoardTitle(String title, int boardColumnSize){
+        System.out.print(" " + title + String.format("%" + (3 * boardColumnSize - title.length() + 2) + "s", ""));
     }
 
-/*
+
+    private void printBoardHeader(int boardColumnSize){
+        System.out.print("   ");
+        for(int j = 0; j < boardColumnSize; j++){
+            System.out.print(" " + (char)(65 + j) + " ");
+        }  
+    }
+
     public void print(){
         System.out.println("\n\n" + name);
         printBoardTitle("Navires:", boardShips[0].length);
@@ -39,19 +43,23 @@ public class Board
         System.out.println();
 
         for(int i = 0; i < boardShips.length; i++){
-            
+            //printing column numbers
             System.out.print(String.format("%2s ", i + 1));
+            //printing board ships
             for(int j = 0; j < boardShips[0].length; j++){
-                System.out.print(" " + printShips(i,j)+ " ");
+                System.out.print(" " + "." + " ");
             }
+            //printing column numbers
             System.out.print(String.format(" %2s ", i + 1));
+            //printing board hits
             for(int j = 0; j < boardHits[0].length; j++){
-                System.out.print(" " + printHits(i,j)  + " ");
+                System.out.print(" " + "." + " ");
             }
             System.out.println();
         }
 
     }
-*/
+
+
 
 }
